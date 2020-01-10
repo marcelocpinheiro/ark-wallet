47a8cb5d8277f11b53b02138f59d3297cc2484e8
Marcelo Pinheiro <marcelompinheiro@outlook.com>
2020-01-10 12:24:57 -0300

    Back button component

---
+++ src/components/common/BackButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="backbutton">
+    <button
+      class="bg-transparent text-blue-500 hover:text-blue-700"
+      @click="back()"
+    >
+      Back
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+
+@Component
+export default class BackButton extends Vue {
+  back() {
+    this.$router.go(-1);
+  }
+}
+</script>
